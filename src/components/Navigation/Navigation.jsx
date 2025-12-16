63d587cba20ef3d3f67ecccaa9e0597e0daf63d2
nyapblesso-a11y <nyapblesso@gmail.com>
2025-12-16 11:47:02 +0100

    the menu button has been implemented the styles well designed

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navigation.css";
 import { useNavigate } from "react-router-dom";
+import { FiMenu } from "react-icons/fi";
+import { FiX } from "react-icons/fi";
+
 function Navigation() {
   const navigate = useNavigate();
+  const [toggle, setToggle] = useState(false);
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
 
   return (
     <>
-      <div className="navigation">
+      <div className={toggle ? "navbar expanded" : "navbar"}>
         <div className="nav-bar">
           <img src="./africalogo.png" alt=" africa" />
-          <ul>
+          <ul className="links">
             <li onClick={() => navigate("/")}>Home</li>
             <li onClick={() => navigate("/about_us")}>About</li>
             <li onClick={() => navigate("/packages")}>Tour-Package</li>
@@ -17,13 +25,15 @@ function Navigation() {
             <li onClick={() => navigate("/contact_us")}>Contact us</li>
           </ul>
         </div>
-
         <div className="button">
           <button className="btn">Sign In</button>
+        </div>
+
+        <div className="toggle-icon" onClick={handleToggle}>
+          {toggle ? <FiX size={40}  className="icons"/> : <FiMenu size={40}  className="icons"/>}
         </div>
       </div>
     </>
   );
 }
 export default Navigation;
-
